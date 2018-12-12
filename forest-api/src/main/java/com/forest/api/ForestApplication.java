a8b2cd94a65b0322f01fa54a18f6388da26bdc15
forest <forest@192.168.226.1>
2018-12-12 11:34:13 +0800

    增加hystrix-dashboard熔断仪表盘

--- forest-api/src/main/java/com/forest/api/ForestApplication.java
+++ forest-api/src/main/java/com/forest/api/ForestApplication.java
@@ -2,8 +2,13 @@ package com.forest.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 
 /**
  * 
@@ -13,9 +18,20 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableEurekaClient
 @SpringBootApplication
 @EnableFeignClients
+@EnableHystrixDashboard
 public class ForestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForestApplication.class, args);
 	}
+	
+	@Bean
+    public ServletRegistrationBean<HystrixMetricsStreamServlet> getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<HystrixMetricsStreamServlet>(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 }
